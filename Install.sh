7f0f5f9cced6c5dfa5b272b79e0a0c7a1a1bad9c
Jefferson <64099400+universoflix@users.noreply.github.com>
2023-08-06 11:22:29 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -11,12 +11,11 @@ wget -O app.py https://raw.githubusercontent.com/universoflix/script/main/app.py
     echo "Instalando Python 3..."
     sudo apt update
     sudo apt install python3.7 -y
-fi
+    
     echo "Instalando Flask e suas dependências..."
     sudo apt update
     sudo apt install python3-flask -y
     sudo pip3 install flask_cors
-fi
 
 # Executar o arquivo
 echo "Executando o arquivo app.py..."
