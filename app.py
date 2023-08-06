9188ce8dfbea3ccf9259ce960a931e0de7493dd4
Jefferson <64099400+universoflix@users.noreply.github.com>
2023-08-06 10:05:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def create_account():
     return jsonify({"message": "Conta criada com sucesso!"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=45678)
+    app.run(host='0.0.0.0', port=45678) 
