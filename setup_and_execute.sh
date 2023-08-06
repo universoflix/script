35f35f2019f6350d5e84181e454b3474ff4b27b3
Jefferson <64099400+universoflix@users.noreply.github.com>
2023-08-06 01:42:47 -0300

    Create setup_and_execute.sh

---
+++ setup_and_execute.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Função para verificar se um pacote está instalado
+check_package() {
+    dpkg -s $1 &> /dev/null
+}
+
+# Baixar o arquivo Python do Git
+wget -O POST.py https://raw.githubusercontent.com/universoflix/script/main/POST.py
+
+# Verificar se o Python 3 está instalado
+if ! check_package "python3"; then
+    echo "Instalando Python 3..."
+    sudo apt update
+    sudo apt install python3 -y
+fi
+
+# Verificar se o Flask está instalado
+if ! python3 -c "import flask" &> /dev/null; then
+    echo "Instalando Flask e suas dependências..."
+    sudo apt update
+    sudo apt install python3-flask -y
+fi
+
+# Dar permissões de execução ao arquivo
+chmod +x POST.py
+
+# Executar o arquivo
+echo "Executando o arquivo POST.py..."
+./POST.py
+
