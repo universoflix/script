336fa342b7e62e57334ea165bc53c30e7e5ec5f9
Jefferson <64099400+universoflix@users.noreply.github.com>
2023-08-06 02:26:17 -0300

    Update setup_and_execute_uwsgi.sh

--- setup_and_execute_uwsgi.sh
+++ setup_and_execute_uwsgi.sh
@@ -12,22 +12,16 @@ if ! check_package "python3"; then
     sudo apt install python3 -y
 fi
 
+# Instalar o uWSGI e Flask usando o pip
+if ! python3 -m pip show uwsgi &> /dev/null; then
+    echo "Instalando uWSGI e Flask..."
+    sudo apt update
+    sudo apt install python3-pip -y
+    sudo python3 -m pip install uwsgi flask
+fi
+
 # Baixar o arquivo app.py
 wget -O app.py https://raw.githubusercontent.com/universoflix/script/main/app.py
-
-# Instalar o uWSGI e Flask
-if ! check_package "uwsgi"; then
-    echo "Instalando uWSGI..."
-    sudo apt update
-    sudo apt install uwsgi-plugin-python3 -y
-    sudo apt install uwsgi -y
-fi
-
-if ! python3 -c "import flask" &> /dev/null; then
-    echo "Instalando Flask e suas dependências..."
-    sudo apt update
-    sudo apt install python3-flask -y
-fi
 
 # Executar o uWSGI
 echo "Executando o uWSGI..."
