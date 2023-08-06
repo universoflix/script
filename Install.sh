65be98b995098d2dcfc8f1cf87cf1ec53a5db32c
Jefferson <64099400+universoflix@users.noreply.github.com>
2023-08-06 11:21:32 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -8,15 +8,10 @@ check_package() {
 # Baixar o arquivo 'app.py' do Git
 wget -O app.py https://raw.githubusercontent.com/universoflix/script/main/app.py
 
-# Verificar se o Python 3 está instalado
-if ! check_package "python3"; then
     echo "Instalando Python 3..."
     sudo apt update
     sudo apt install python3.7 -y
 fi
-
-# Verificar se o Flask está instalado
-if ! python3 -c "import flask" &> /dev/null; then
     echo "Instalando Flask e suas dependências..."
     sudo apt update
     sudo apt install python3-flask -y
