07bba01a9733e338cd7e080ba83ab15a64642636
Jefferson <64099400+universoflix@users.noreply.github.com>
2023-08-05 23:58:20 -0300

    Update install_wireguard_web.sh

--- install_wireguard_web.sh
+++ install_wireguard_web.sh
@@ -88,6 +88,7 @@ if __name__ == '__main__':
     # Configurar as permissões corretas para o arquivo wg0.conf
     subprocess.run(['chmod', '600', '/etc/wireguard/wg0.conf'])
     app.run(host='0.0.0.0', port=5000, debug=True)
+
 EOF 
 
 # Criar o arquivo index.html com o conteúdo da página HTML
